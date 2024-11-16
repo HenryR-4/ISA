9b11d2db8ebe77ea5cd84196b764e2a70f34ed1f
HenryR-4 <chr0005@uah.edu>
2024-11-15 22:32:22 -0600

    updated assembler

--- assembler.py
+++ assembler.py
@@ -1,7 +1,6 @@
 import sys
 
 '''
-
 This is a very basic assembler which takes a text file as input and ouputs the binary version to another file
 
 supports line comments prefixed with '/', it also ignores anything past the instruction
@@ -10,13 +9,19 @@ supports labels, so long as they are on the same line as an instruction
 
 it does not support any error handling besides very basic syntax
 
+it supports a optional data section at the top, use a tag called "start" at the program
+entry point if you include data otherwise it is not required
+
+first line of the output will always be the address of the start tag which the pc
+is set to at the start of the program
+
 to use:
 	python3 assembler.py [input file] [output file]
 
 '''
 
 labels = {
-	"start": 0,
+	"start": 1,
 }
 
 opcodes = {
@@ -60,109 +65,119 @@ outputfile = sys.argv[2]
 print("Outputing to " + outputfile)
 of = open(outputfile, "w")
 
-lineNumber = 0
+program_str = ""
+
+lineNumber = 1 # start line number at 1 because first instruction will be inserted after  
 # parse file initially to find any labels, this is not efficient but it doesn't matter for the size of programs
 # i'm creating
 for line in f:
 	words = line.split()
 	if(line.strip() and words[0][0] != '/'): # only do if not an empty line or comment
-		if(opcodes.get(words[0], -1) == -1): # if first word in line is not opcode then it must be a label
+		if(opcodes.get(words[0], -1) == -1 and not words[0].lstrip('-').isnumeric()): # if first word in line is not opcode and isn't a number then it must be a label
 			labels.update({words[0] : lineNumber})
 		lineNumber+=1
 
 f.seek(0)
 
 # read file line by line and process
-lineNumber = 0
+lineNumber = 1 # start line number at 1 because first instruction will be inserted after  
 for line in f:
 	words = line.split()
 	if(line.strip() and words[0][0] != '/'): # only do if not an empty line or comment
-		if(opcodes.get(words[0], -1) == -1): # if first word in line is not opcode then it must be a label
+		if(labels.get(words[0], -1) != -1): # if first word in line is a label
 			opcode = words[1] # opcode will be next word
 			i = 1 # index of opcode word
 		else:
 			opcode = words[0]
 			i = 0 # index of opcode word
 
-		# get numerical opcode
-		opcode = opcodes.get(opcode, -1)
-		if(opcode == -1):
-			print("OP-CODE not found instruction #: " + lineNumber)
-			exit()
-
-		if(opcode == 0 or opcode == 14 or opcode == 15):
-			print("{:016b}".format(opcode<<12))
-			of.write("{:016b}\n".format(opcode<<12))
-
-		# Handles add through cmp, and Memory Instructions
-		elif((opcode >= 0 and opcode <= 7) or opcode >= 12):
-			# get reg1
-			reg1 = registers.get(words[i+1], -1)
-			if(reg1 == -1):
-				print("Invalid Register instruction #: " + lineNumber)
+		if(lineNumber >= labels.get("start")):
+			# get numerical opcode
+			opcode = opcodes.get(opcode, -1)
+			if(opcode == -1):
+				print("OP-CODE not found instruction #: " + lineNumber)
 				exit()
-			# handle I-type
-			if(words[i+2][0] == '#'):
-				immediate = int(words[i+2][1:len(words[i+2])])
-				print("{:04b}_1_{:03b}_{:08b}".format(opcode,reg1,immediate))
-				of.write("{:04b}_1_{:03b}_{:08b}\n".format(opcode,reg1,immediate))
-			# handle R-type with offset
-			elif(words[i+2][0] == '('):
-				close_index = words[i+2].find(')')
-				if(close_index == -1):
-					print("Missing ) instruction #: " + lineNumber)
-					exit()
-				offset = int(words[i+2][1:close_index])
-				reg2 = words[i+2][close_index+1:len(words[i+2])]
-				reg2 = registers.get(reg2, -1)
-				if(reg2 == -1):
-					print("Invalid Register instruction #: " + lineNumber)
-					exit()
-				print("{:04b}_0_{:03b}_{:03b}_{:05b}".format(opcode,reg1,reg2,offset))
-				of.write("{:04b}_0_{:03b}_{:03b}_{:05b}\n".format(opcode,reg1,reg2,offset))
-			else:
-				reg2 = words[i+2][0:len(words[i+2])]
-				reg2 = registers.get(reg2, -1)
-				if(reg2 == -1):
-					print("Invalid Register instruction #: " + lineNumber)
-					exit()
-				print("{:04b}_0_{:03b}_{:03b}_00000".format(opcode,reg1,reg2))
-				of.write("{:04b}_0_{:03b}_{:03b}_00000\n".format(opcode,reg1,reg2))
 
-		# Handle Branch Instructions
-		#elif(opcode >= 8 and opcode <= 11):
-		else:
-			if(words[i+1][0] == '('):
-				close_index = words[i+1].find(')')
-				if(close_index == -1):
-					print("Missing ) instruction #: " + lineNumber)
-					exit()
-				immediate = int(words[i+1][1:close_index])
-				reg1 = words[i+1][close_index+1:len(words[i+1])]
-				reg1 = registers.get(reg1, -1)
+			if(opcode == 0 or opcode == 14 or opcode == 15):
+				program_str += "{:016b}\n".format(opcode<<12)
+
+			# Handles add through cmp, and Memory Instructions
+			elif((opcode >= 0 and opcode <= 7) or opcode >= 12):
+				# get reg1
+				reg1 = registers.get(words[i+1], -1)
 				if(reg1 == -1):
 					print("Invalid Register instruction #: " + lineNumber)
 					exit()
-				print("{:04b}_1_{:03b}_{:08b}".format(opcode,reg1,immediate))
-				of.write("{:04b}_1_{:03b}_{:08b}\n".format(opcode,reg1,immediate))
-			elif(words[i+1][0] == '#'):
-				address = words[i+1][1:len(words[i+1])]
-				if(labels.get(address, -1) == -1):
-					address = int(address)
+				# handle I-type
+				if(words[i+2][0] == '#'):
+					immediate = int(words[i+2][1:len(words[i+2])])
+					if(immediate < 0):
+						immediate = immediate & 0x00FF 
+					program_str += "{:04b}_1_{:03b}_{:08b}\n".format(opcode,reg1,immediate)
+
+				# handle R-type with offset
+				elif(words[i+2][0] == '('):
+					close_index = words[i+2].find(')')
+					if(close_index == -1):
+						print("Missing ) instruction #: " + lineNumber)
+						exit()
+					offset = int(words[i+2][1:close_index])
+					reg2 = words[i+2][close_index+1:len(words[i+2])]
+					reg2 = registers.get(reg2, -1)
+					if(reg2 == -1):
+						print("Invalid Register instruction #: " + lineNumber)
+						exit()
+					if(offset < 0):
+						offset = offset & 0x001F
+					program_str += "{:04b}_0_{:03b}_{:03b}_{:05b}\n".format(opcode,reg1,reg2,offset)
 				else:
-					address = labels.get(address)
-				print("{:04b}_0_{:011b}".format(opcode,address))
-				of.write("{:04b}_0_{:011b}\n".format(opcode,address))
+					reg2 = words[i+2][0:len(words[i+2])]
+					reg2 = registers.get(reg2, -1)
+					if(reg2 == -1):
+						print("Invalid Register instruction #: " + lineNumber)
+						exit()
+					program_str += "{:04b}_0_{:03b}_{:03b}_00000\n".format(opcode,reg1,reg2)
+
+			# Handle Branch Instructions
 			else:
-				reg1 = words[i+1]
-				reg1 = registers.get(reg1, -1)
-				if(reg1 == -1):
-					print("Invalid Register instruction #: " + lineNumber)
-					exit()
-				print("{:04b}_1_{:03b}_{:08b}".format(opcode,reg1,0))
-				of.write("{:04b}_1_{:03b}_{:08b}\n".format(opcode,reg1,0))
+				if(words[i+1][0] == '('):
+					close_index = words[i+1].find(')')
+					if(close_index == -1):
+						print("Missing ) instruction #: " + lineNumber)
+						exit()
+					immediate = int(words[i+1][1:close_index])
+					reg1 = words[i+1][close_index+1:len(words[i+1])]
+					reg1 = registers.get(reg1, -1)
+					if(reg1 == -1):
+						print("Invalid Register instruction #: " + lineNumber)
+						exit()
+					if(immediate < 0):
+						immediate = immediate & 0x00FF
+					program_str += "{:04b}_1_{:03b}_{:08b}\n".format(opcode,reg1,immediate)
+				elif(words[i+1][0] == '#'):
+					address = words[i+1][1:len(words[i+1])]
+					if(labels.get(address, -1) == -1):
+						address = int(address)
+					else:
+						address = labels.get(address)
+					program_str += "{:04b}_0_{:011b}\n".format(opcode,address)
+				else:
+					reg1 = words[i+1]
+					reg1 = registers.get(reg1, -1)
+					if(reg1 == -1):
+						print("Invalid Register instruction #: " + lineNumber)
+						exit()
+					program_str += "{:04b}_1_{:03b}_{:08b}\n".format(opcode,reg1,0)
+		else:
+			opcode = int(opcode)
+			if(opcode < 0):
+				opcode = opcode & 0xFFFF
+			program_str += "{:016b}\n".format(int(opcode))
 
 		# only update line number if instruction is present
 		lineNumber+=1 
 
 print(labels)
+program_str = "{:016b}\n".format(labels.get("start")) + program_str
+print(program_str)
+of.write(program_str)
